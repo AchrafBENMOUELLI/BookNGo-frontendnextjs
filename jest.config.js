316d1403fc3d41bc7d54bc8a5f2a9e74d69712cb
Achraf BENMOUELLI <achraf.BENMOUELLI@esprit.tn>
2026-07-02 23:29:38 +0100

    fix: rename jest config to js to avoid ts-node requirement

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'jsdom',
   setupFiles: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
@@ -21,4 +20,4 @@ const config: Config = {
   ],
 };
 
-export default config;
+module.exports = config;
